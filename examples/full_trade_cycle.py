65e41d60fc21e137812f3dd66f63ea8c8f6396e4
snub-fighter <demers.nathan@gmail.com>
2019-06-09 08:15:01 -0700

    Improved stability
    Updated to search for open orders properly.
    Changed case from orderid to orderId
    Added receive window for stability.

--- examples/full_trade_cycle.py
+++ examples/full_trade_cycle.py
@@ -2,13 +2,15 @@ from bitrue.client import Client
 from tabulate import tabulate
 from pprint import pprint
 import os
+import time
 
 if __name__ == '__main__':
-    client = Client(api_key='2f0982022fc09377f43bc1da4bcb6c2219778ffe256cb96d0e966362908f0d51',
-                    api_secret='8ec2561b0b457b6b1695f4bc6e95eb11663723fe510e39baed92998187075150',
+    RECV_WINDOW = 6000000
+    client = Client(api_key='',
+                    api_secret='',
                     )
 
-    sell_price = .470
+    sell_price = 1.5
     sell_qty = 5
     buyback_price = .40
 
@@ -19,16 +21,17 @@ if __name__ == '__main__':
     create_order = client.order_limit_sell(symbol='XRPUSDT', quantity=sell_qty, price=sell_price)
     pprint(create_order)
     orderId_Sell = create_order['orderId']
-
+    print(type(orderId_Sell))
     while True:
         #check order status
-        orderstatus = client.get_open_orders(symbol='XRPUSDT', orderid=orderId_Sell)
-        if orderstatus[0]['status'] == 'FILLED':
+        orderstatus = client.get_order(symbol='XRPUSDT', orderId=orderId_Sell,recvWindow=RECV_WINDOW)
+        if orderstatus['status'] == 'FILLED':
             #buy back using gains
             buyback_order = client.order_limit_buy(symbol='XRPUSDT', quantity=buyback_qty, price=buyback_price)
         else:
             os.system('cls' if os.name == 'nt' else 'clear')  # For Windows
             print('OrderId: {} - still open'.format(orderId_Sell))
+            time.sleep(1)
 '''
 
 {'clientOrderId': '',
